a1b57e1b35ad2122eb33ce91bb01cdb753aebd15
wilknisoliveira <wilknisoliveira@gmail.com>
2023-06-30 15:36:26 -0300

    feat: more implementations of math operations

--- rest-spring-kotlin/src/main/kotlin/com/springkotlin/MathController.kt
+++ rest-spring-kotlin/src/main/kotlin/com/springkotlin/MathController.kt
@@ -5,18 +5,37 @@ import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import java.util.concurrent.atomic.AtomicLong
+import kotlin.math.sqrt
 
 @RestController
 class MathController {
     val counter: AtomicLong = AtomicLong()
 
-    @RequestMapping(value = ["/sum/{numberOne}/{numberTwo}"])
+    @RequestMapping(value = ["/math/{numberOne}/{mathOperation}/{numberTwo}"])
     fun sum(@PathVariable(value = "numberOne") numberOne: String?,
+            @PathVariable(value= "mathOperation") mathOperation: String?,
             @PathVariable(value = "numberTwo") numberTwo: String?
     ): Double{
         if(!isNumeric(numberOne) || !isNumeric(numberTwo))
             throw UnsupportedMathOperationException("Please set a valid number")
-        return convertToDouble(numberOne) + convertToDouble(numberTwo)
+
+        when(mathOperation){
+            "+" -> return convertToDouble(numberOne) + convertToDouble(numberTwo)
+            "-" -> return convertToDouble(numberOne) - convertToDouble(numberTwo)
+            "*" -> return convertToDouble(numberOne) * convertToDouble(numberTwo)
+            "x" -> return convertToDouble(numberOne) * convertToDouble(numberTwo)
+            "X" -> return convertToDouble(numberOne) * convertToDouble(numberTwo)
+            "/" -> return convertToDouble(numberOne) / convertToDouble(numberTwo)
+            "avg" -> return (convertToDouble(numberOne) + convertToDouble(numberTwo))/2
+            else -> throw UnsupportedMathOperationException("Please set a valid math operation")
+        }
+    }
+
+    @RequestMapping(value = ["/math/sqrt/{number}"])
+    fun squareRoot(@PathVariable(value="number") number: String?): Double{
+        if(!isNumeric(number))
+            throw UnsupportedMathOperationException("Please set a valid number")
+        return sqrt(convertToDouble(number))
     }
 
     private fun isNumeric(number: String?): Boolean{
